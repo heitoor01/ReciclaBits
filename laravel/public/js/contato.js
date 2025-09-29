f462b0bc683bc8ad766a7620571183cbf23b5f6e
docker <docker@docker.my.domain>
2025-09-29 10:42:09 -0400

    tá apiado é meu chefe?

--- laravel/public/js/contato.js
+++ laravel/public/js/contato.js
@@ -1,3 +1,28 @@
+
+function faq() {
+    window.location.href = "/faq"
+}
+
+function suggestions() {
+    window.location.href = "/sugestoes"
+}
+
+function goHome() {
+    window.location.href = "/"
+};
+
+function talkToUs() {
+    window.location.href = "/fale-conosco"
+};
+
+function signUp() {
+  window.location.href = "/cadastro"
+};
+
+function recPass() {
+    window.location.href = "/recuperar-senha"
+};
+
 const questions = document.querySelectorAll('.faq-question');
 questions.forEach(q => {
     q.addEventListener('click', () => {
